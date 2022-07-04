d42f1e5ef6068aa4cab57a589f16f841879a7ba0
Thorsten Ludewig <th@l9g.de>
2022-07-04 12:45:10 +0200

    seperate config and app from index.html into files

---
+++ app.js
@@ -0,0 +1,155 @@
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+var toastMessage = document.getElementById('toastMessage')
+var lastJpegImagePath = "";
+
+function clearTbody(elementId) {
+  const curTbody = document.getElementById(elementId)
+  const tbody = document.createElement('tbody');
+  tbody.id = elementId;
+  curTbody.parentNode.replaceChild(tbody, curTbody)
+  return tbody;
+}
+
+function setGlobalJpegPath(images) {
+  var exifJpegPath = document.getElementById('exifJpegPath')
+  var gp2FileExifButton = document.getElementById('gp2FileExifButton')
+
+  images.forEach(element => {
+    let path = element.info.path.toUpperCase();
+    if (path.endsWith(".JPG")) {
+      lastJpegImagePath = element.info.path;
+      exifJpegPath.innerHTML = " : " + lastJpegImagePath;
+      gp2FileExifButton.disabled = false;
+    }
+  })
+}
+
+function gp2AutoDetect() {
+  console.log("gp2AutoDetect");
+  const tbody = clearTbody("gp2AutoDetectTableBody");
+  var counter = 1;
+
+  fetch(gphoto2_webapi_url + "/api/auto-detect").then((resp) => resp.json()).then(function (data) {
+    if (data.return_code == 0) {
+      data.result.forEach(element => {
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = (counter++).toString();
+        row.insertCell(1).innerHTML = element.model;
+        row.insertCell(2).innerHTML = element.port;
+      });
+    }
+    else {
+      var toast = new bootstrap.Toast(toastLiveExample);
+      toastMessage.innerHTML = "return_code = " + data.return_code;
+      toast.show();
+    }
+  });
+}
+
+function gp2Version() {
+  console.log("gp2Version");
+  const tbody = clearTbody("gp2VersionTableBody");
+
+  fetch(gphoto2_webapi_url + "/api/version").then((resp) => resp.json()).then(function (data) {
+    data.result.forEach(element => {
+      var row = tbody.insertRow();
+      row.insertCell(0).innerHTML = element.name;
+      row.insertCell(1).innerHTML = element.version;
+      row.insertCell(2).innerHTML = (element.libs === undefined) ? "-" : element.libs;
+    });
+  });
+}
+
+function gp2TriggerCapture() {
+  console.log("gp2TriggerCapture");
+  fetch(gphoto2_webapi_url + "/api/trigger-capture").then((resp) => resp.json()).then(function (data) {
+    if (data.return_code == 0) {
+      var toast = new bootstrap.Toast(toastLiveExample);
+      toastMessage.innerHTML = "Trigger capture succeed!";
+      toast.show();
+    }
+    else {
+      var toast = new bootstrap.Toast(toastLiveExample);
+      toastMessage.innerHTML = "return_code = " + data.return_code;
+      toast.show();
+    }
+  });
+}
+
+function gp2Shutdown() {
+  console.log("gp2Shutdown");
+  fetch(gphoto2_webapi_url + "/api/server/shutdown").then((resp) => resp.json()).then(function (data) {
+    if (data.return_code == 0) {
+      var toast = new bootstrap.Toast(toastLiveExample);
+      toastMessage.innerHTML = "Server shutdown succeed!";
+      toast.show();
+    }
+    else {
+      var toast = new bootstrap.Toast(toastLiveExample);
+      toastMessage.innerHTML = "return_code = " + data.return_code;
+      toast.show();
+    }
+  });
+}
+
+function gp2CaptureImage() {
+  console.log("gp2CaptureImage");
+  var button = document.getElementById("gp2CaptureImageButton");
+  var loading = document.getElementById("gp2CaptureImageLoading");
+  loading.style = "display: inline-block";
+  button.disabled = true;
+  fetch(gphoto2_webapi_url + "/api/capture-image").then((resp) => resp.json()).then(function (data) {
+    button.disabled = false;
+    loading.style = "display: none";
+    if (data.return_code == 0) {
+      setGlobalJpegPath(data.images);
+      var toast = new bootstrap.Toast(toastLiveExample);
+      toastMessage.innerHTML = "capture image succeed!";
+      toast.show();
+    }
+    else {
+      var toast = new bootstrap.Toast(toastLiveExample);
+      toastMessage.innerHTML = "return_code = " + data.return_code;
+      toast.show();
+    }
+  });
+}
+
+function gp2CaptureImageDownload() {
+  console.log("gp2CaptureImageDownload");
+  var button = document.getElementById("gp2CaptureImageDownloadButton");
+  var loading = document.getElementById("gp2CaptureImageDownloadLoading");
+  loading.style = "display: inline-block";
+  button.disabled = true;
+  fetch(gphoto2_webapi_url + "/api/capture-image-download").then((resp) => resp.json()).then(function (data) {
+    button.disabled = false;
+    loading.style = "display: none";
+    if (data.return_code == 0) {
+      setGlobalJpegPath(data.images);
+      var toast = new bootstrap.Toast(toastLiveExample);
+      toastMessage.innerHTML = "capture image and download succeed!";
+      toast.show();
+    }
+    else {
+      var toast = new bootstrap.Toast(toastLiveExample);
+      toastMessage.innerHTML = "return_code = " + data.return_code;
+      toast.show();
+    }
+  });
+}
+
+function gp2FileExif() {
+  console.log("gp2FileExif");
+  const tbody = clearTbody("gp2ExifTableBody");
+
+  fetch(gphoto2_webapi_url + "/api/file/exif" + lastJpegImagePath).then((resp) => resp.json()).then(function (data) {
+    Object.entries(data).forEach(
+      ([k, v]) => {
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = k;
+        row.insertCell(1).innerHTML = v;
+      }
+    );
+  });
+}
---
+++ config.js
@@ -0,0 +1,3 @@
+
+// change this to your gphoto2-webapi server url
+var gphoto2_webapi_url = "http://192.168.4.130:8866";
